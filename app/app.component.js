9da1d2aedcf92974a9f3afd28b266b43916c4428
ikepon <tatsuyanoheya@gmail.com>
2016-11-03 14:58:41 +0900

    template をバッククオートを使って読みやすく

--- app/app.component.js
+++ app/app.component.js
@@ -26,7 +26,7 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2><div><label>id: </label>{{hero.id}}</div><div><label>name: </label>{{hero.name}}</div>'
+            template: "\n    <h1>{{title}}</h1>\n    <h2>{{hero.name}} details!</h2>\n    <div><label>id: </label>{{hero.id}}</div>\n    <div><label>name: </label>{{hero.name}}</div>\n    "
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
