e3ca5289ea73c812c9e2e3bd196c5c8d27513152
ikepon <tatsuyanoheya@gmail.com>
2016-11-03 14:38:35 +0900

    App name と hero name を表示

--- app/app.component.js
+++ app/app.component.js
@@ -11,11 +11,13 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var AppComponent = (function () {
     function AppComponent() {
+        this.title = 'Tour of Heroes';
+        this.hero = 'Windstorm';
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<h1>My First Angular App</h1>'
+            template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>'
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
