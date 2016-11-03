f9dfadc10e423e674d2fafb88a075e45fd9b06ad
ikepon <tatsuyanoheya@gmail.com>
2016-11-03 21:20:50 +0900

    Cosmetic

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,12 +7,14 @@ import { Hero } from './hero';
     <h1>{{title}}</h1>
     <h2>My Heroes</h2>
     <ul class="heroes">
-      <li *ngFor="let hero of heroes" [class.selected]="hero === selectedHero" (click)="onSelect(hero)">
+      <li *ngFor="let hero of heroes"
+        [class.selected]="hero === selectedHero"
+        (click)="onSelect(hero)">
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
     <my-hero-detail [hero]="selectedHero"></my-hero-detail>
-    `,
+  `,
   styles: [`
     .selected {
       background-color: #CFD8DC !important;
@@ -66,9 +68,8 @@ import { Hero } from './hero';
 
 export class AppComponent {
   title = 'Tour of Heroes';
-  selectedHero: Hero;
   heroes = HEROES;
-
+  selectedHero: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
