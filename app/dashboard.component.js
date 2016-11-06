a1eb6d5eedab5152da92cbf435b893fbb7b9e7f8
ikepon <tatsuyanoheya@gmail.com>
2016-11-06 19:05:31 +0900

    DashboardからHero詳細を表示

--- app/dashboard.component.js
+++ app/dashboard.component.js
@@ -9,9 +9,11 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var router_1 = require('@angular/router');
 var hero_service_1 = require('./hero.service');
 var DashboardComponent = (function () {
-    function DashboardComponent(heroService) {
+    function DashboardComponent(router, heroService) {
+        this.router = router;
         this.heroService = heroService;
         this.heroes = [];
     }
@@ -20,14 +22,20 @@ var DashboardComponent = (function () {
         this.heroService.getHeroes()
             .then(function (heroes) { return _this.heroes = heroes.slice(1, 5); });
     };
-    DashboardComponent.prototype.gotoDetail = function (hero) { };
+    // The gotoDetail method navigates in two steps:
+    // 1. Set a route link parameters array
+    // 2. Pass the array to the router's navigate method
+    DashboardComponent.prototype.gotoDetail = function (hero) {
+        var link = ['/detail', hero.id];
+        this.router.navigate(link);
+    };
     DashboardComponent = __decorate([
         core_1.Component({
             moduleId: module.id,
             selector: 'my-dashboard',
             templateUrl: 'dashboard.component.html',
         }), 
-        __metadata('design:paramtypes', [hero_service_1.HeroService])
+        __metadata('design:paramtypes', [router_1.Router, hero_service_1.HeroService])
     ], DashboardComponent);
     return DashboardComponent;
 }());
--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
@@ -12,12 +13,21 @@ import { HeroService } from './hero.service';
 export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService) { }
+  constructor(
+    private router: Router,
+    private heroService: HeroService
+  ) { }
 
   ngOnInit(): void {
     this.heroService.getHeroes()
       .then(heroes => this.heroes = heroes.slice(1, 5));
   }
 
-  gotoDetail(hero: Hero): void { /* not implemented yet */}
+  // The gotoDetail method navigates in two steps:
+  // 1. Set a route link parameters array
+  // 2. Pass the array to the router's navigate method
+  gotoDetail(hero: Hero): void {
+    let link = ['/detail', hero.id];
+    this.router.navigate(link);
+  }
 }
