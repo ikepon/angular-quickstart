171ace5eaf11a20b5539f88c74ed53cf643b8e94
ikepon <tatsuyanoheya@gmail.com>
2016-12-04 14:12:12 +0900

    app.component.htmlを追加

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,7 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: `
-    <h1>{{title}}</h1>
-    <nav>
-      <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
-      <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
-    </nav>
-    <router-outlet></router-outlet>
-  `
+  templateUrl: 'app/app.component.html',
 })
 export class AppComponent {
   title = 'Tour of Heroes';
