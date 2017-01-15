6788c30d7e2e3e2ae599cc879d7b82dcb0dcfe48
ikepon <tatsuyanoheya@gmail.com>
2017-01-15 16:38:43 +0900

    変数を定義してcomponentに表示

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: '<h1>My First Angular App</h1>'
+  template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>'
 })
-export class AppComponent { }
+export class AppComponent {
+  title = 'Tour of Heroes';
+  hero = 'Windstorm';
+}
