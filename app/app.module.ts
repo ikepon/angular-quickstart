2e00de3269089028990a75abca400eb9e4eaa377
ikepon <tatsuyanoheya@gmail.com>
2017-01-15 16:56:00 +0900

    2way binding のためにforms module を読み込む

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,17 @@
 import { NgModule }      from '@angular/core';
 // ブラウザで表示させるために必要
 import { BrowserModule } from '@angular/platform-browser';
+// 2way binding のために必要
+import { FormsModule }   from '@angular/forms';
 
 // AppComponent を読みこんでる
 import { AppComponent }   from './app.component';
+
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [
+    BrowserModule,
+    FormsModule
+  ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
