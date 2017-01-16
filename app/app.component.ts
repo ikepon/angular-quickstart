9ae5642f84a234c3d8e5377d60fc43c1f311bb8a
ikepon <tatsuyanoheya@gmail.com>
2017-01-17 08:46:11 +0900

    Heros を読み込み、heroesパラメータにHeroクラスの配列を入れる

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,19 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero';
 
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
-
 @Component({
   selector: 'my-app',
   template: `
@@ -78,7 +65,7 @@ const HEROES: Hero[] = [
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
   onSelect(hero: Hero): void {
