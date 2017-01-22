a9a4b506be8421d580836076d7a78046f72787bd
ikepon <tatsuyanoheya@gmail.com>
2017-01-22 18:26:18 +0900

    dashboardとhero-detailのスタイルを調整

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,22 +1,19 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
 
 import 'rxjs/add/operator/switchMap';
 
-import { Hero } from './hero';
 import { HeroService } from './hero.service';
 
 @Component({
   moduleId: module.id,
   selector: 'my-hero-detail',
   templateUrl: 'hero-detail.component.html',
+  styleUrls: [ 'hero-detail.component.css' ]
 })
 
 export class HeroDetailComponent implements OnInit {
-  @Input()
-  hero: Hero;
-
   ngOnInit(): void {
     this.route.params
       .switchMap((params: Params) => this.heroService.getHero(+params['id']))
