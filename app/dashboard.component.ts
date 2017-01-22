c3a1dc95c43494e1e80f9819aa8b566b85e28c17
ikepon <tatsuyanoheya@gmail.com>
2017-01-22 14:21:05 +0900

    Dashboard にtop heroesを表示

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
+  moduleId: module.id,
   selector: 'my-dashboard',
-  template: `
-    <h3>My Dashboard</h3>
-  `
+  templateUrl: 'dashboard.component.html'
 })
 
-export class DashboardComponent {}
+export class DashboardComponent {
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit(): void {
+    this.heroService.getHeroes()
+      .then(heroes => this.heroes = heroes.slice(1, 5));
+  }
+}
