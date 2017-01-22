fd0ba8d97aacbed87d6b1d6a72f8b5e35cb941cd
ikepon <tatsuyanoheya@gmail.com>
2017-01-22 13:05:31 +0900

    app.component を heroes.componentにrename

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -3,7 +3,7 @@ import { Hero } from './hero';
 import { HeroService } from './hero.service';
 
 @Component({
-  selector: 'my-app',
+  selector: 'my-heroes',
   template: `
     <h1>{{title}}</h1>
     <h2>My Heroes</h2>
@@ -67,7 +67,7 @@ import { HeroService } from './hero.service';
 })
 
 
-export class AppComponent implements OnInit {
+export class HeroesComponent implements OnInit {
   title = 'Tour of Heroes';
   heroes: Hero[];
   selectedHero: Hero;
