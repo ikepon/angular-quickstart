1f49f40a506902357595b13c517b6a21fc258e22
ikepon <tatsuyanoheya@gmail.com>
2017-01-22 16:29:15 +0900

    Routingを別ファイル化

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,9 +1,6 @@
 import { NgModule }      from '@angular/core';
-// ブラウザで表示させるために必要
 import { BrowserModule } from '@angular/platform-browser';
-// 2way binding のために必要
 import { FormsModule }   from '@angular/forms';
-import { RouterModule }  from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import { DashboardComponent } from './dashboard.component';
@@ -11,29 +8,13 @@ import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from './hero.service';
 
+import { AppRoutingModule } from './app-routing.module';
+
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([
-      {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      },
-      {
-        path: 'heroes',
-        component: HeroesComponent
-      },
-      {
-        path: 'detail/:id',
-        component: HeroDetailComponent
-      }
-    ])
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
