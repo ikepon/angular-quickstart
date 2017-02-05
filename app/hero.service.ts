1a4be95807d430a00abbdb136258e3007542370c
ikepon <tatsuyanoheya@gmail.com>
2017-02-05 09:42:38 +0900

    Heroを削除

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -38,6 +38,13 @@ export class HeroService {
       .then(() => hero)
       .catch(this.handleError);
   }
+  delete(id: number): Promise<void> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete(url, {headers: this.headers})
+      .toPromise()
+      .then(() => null)
+      .catch(this.handleError);
+  }
 
   private handleError(error: any): Promise<any> {
     console.error('An error occurred', error); // for demo purposes only
--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -34,6 +34,14 @@ export class HeroesComponent implements OnInit {
       });
   }
 
+  delete(hero: Hero): void {
+    this.heroService.delete(hero.id)
+      .then(() => {
+        this.heroes = this.heroes.filter(h => h !== hero);
+        if (this.selectedHero === hero) { this.selectedHero = null; }
+      });
+  }
+
   ngOnInit(): void {
     this.getHeroes();
   }
